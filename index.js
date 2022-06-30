7ec1db94bfa885553b130d71efe4bb4b8862bce1
F4RR311 <valery.leo2016@yandex.ru>
2022-07-01 00:51:20 +0300

    uploadFile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
+import multer from 'multer';
 
 const PORT = 3100;
 import {registerValidation, loginValidation, postCreateValidation} from './validadtions.js'
@@ -16,13 +17,32 @@ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.kafjq9l.mongodb.net/blog?r
         console.log('DB Error', err)
     });
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+
+    },
+    filename: (_, file, cb) => {
+        cb(null, 'file.originalname')
+
+    }
+})
+
+const upload = multer({storage})
+
 const app = express();
+
 app.use(express.json());
 
 app.post('/auth/login', login, loginValidation);
 app.get('/auth/me', checkAuth, getMe);
 app.post('/auth/register', registerValidation, register);
 
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+});
 
 app.get('/posts', postCreateValidation, getAll)
 app.get('/posts/:id', getOne)
@@ -32,10 +52,10 @@ app.patch('/posts/:id', checkAuth, updatePost)
 
 app.listen(PORT, (err) => {
 
-
     if (err) {
         return console.log(err)
     }
+
 
     console.log(`App listen on ${PORT} `)
 })
