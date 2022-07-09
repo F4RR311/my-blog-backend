179bedb5d7ec52f0226a4970d6b1313ee8e99717
F4RR311 <valery.leo2016@yandex.ru>
2022-07-09 21:20:43 +0300

    little-fix

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,9 +1,23 @@
 import PostModel from "../models/Post.js";
 
+
 export const getAll = async (req, res) => {
     try {
         const posts = await PostModel.find().populate('user').exec();
         res.json(posts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи'
+        })
+    }
+}
+export const getTags = async (req, res) => {
+    try {
+        const posts = await PostModel.find().limit(5).exec();
+        const tags = posts.map(obj=>obj.tags).flat().slice(0,5)
+
+        res.json(tags)
     } catch (err) {
         console.log(err)
         res.status(500).json({
@@ -40,7 +54,7 @@ export const getOne = async (req, res) => {
                 }
                 return res.json(doc)
             }
-        );
+        ).populate('user');
     } catch (e) {
 
     }
@@ -79,7 +93,7 @@ export const createPost = async (req, res) => {
             title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
-            tags: req.body.tags,
+            tags: req.body.tags.split(","),
             user: req.userId,
         })
         const post = await doc.save();
@@ -101,7 +115,7 @@ export const updatePost = async (req, res) => {
             title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
-            tags: req.body.tags,
+            tags: req.body.tags.split(","),
             user: req.userId,
         });
         res.json({
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 3100;
 import {registerValidation, loginValidation, postCreateValidation} from './validadtions.js'
 import {UserController, PostController} from './controllers/index.js'
 import {validationErrors, checkAuth} from "./utils/index.js";
+import cors from 'cors'
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.kafjq9l.mongodb.net/blog?retryWrites=true&w=majority'
 )
@@ -32,6 +33,7 @@ const upload = multer({storage})
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, validationErrors, UserController.login);
@@ -43,19 +45,20 @@ app.post('/upload', upload.single('image'), (req, res) => {
         url: `/uploads/${req.file.originalname}`,
     })
 });
+app.get('/tags', postCreateValidation, PostController.getTags)
 
 app.get('/posts', postCreateValidation, PostController.getAll)
+app.get('/posts/tags', postCreateValidation, PostController.getTags)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, validationErrors, PostController.createPost)
 app.delete('/posts/:id', checkAuth, PostController.removePost)
 app.patch('/posts/:id', checkAuth, PostController.updatePost)
 
-app.listen(PORT, (err) => {
 
+app.listen(process.env.PORT || 3100, (err) => {
     if (err) {
-        return console.log(err)
+        return console.log(err);
     }
 
-
-    console.log(`App listen on ${PORT} `)
-})
+    console.log('Server OK');
+});
